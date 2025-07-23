983d0cd5b39eb61391aa7174131b9c871e057dc5
Thelightbetween <kassieannk@gmail.com>
2025-07-23 12:12:16 -0500

    updated intcome
    place bubble left side

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        {/* Intercom settings script */}
+        {/* Intercom Messenger full script */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -17,6 +17,35 @@ export default function Document() {
                 horizontal_padding: 20,
                 vertical_padding: 20
               };
+              (function() {
+                var w = window;
+                var ic = w.Intercom;
+                if (typeof ic === "function") {
+                  ic('reattach_activator');
+                  ic('update', window.intercomSettings);
+                } else {
+                  var d = document;
+                  var i = function() { i.c(arguments); };
+                  i.q = [];
+                  i.c = function(args) { i.q.push(args); };
+                  w.Intercom = i;
+                  function l() {
+                    var s = d.createElement('script');
+                    s.type = 'text/javascript';
+                    s.async = true;
+                    s.src = 'https://widget.intercom.io/widget/z240m4p6';
+                    var x = d.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(s, x);
+                  }
+                  if (document.readyState === 'complete') {
+                    l();
+                  } else if (w.attachEvent) {
+                    w.attachEvent('onload', l);
+                  } else {
+                    w.addEventListener('load', l, false);
+                  }
+                }
+              })();
             `
           }}
         />
